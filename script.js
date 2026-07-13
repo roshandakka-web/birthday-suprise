1aff202b3e6727f431eb5fadb7b4f204ffb1a420
roshandakka-web <roshandakka@gmail.com>
2026-07-13 10:49:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+🔒 Connecting...
+
+██████████
+
+🌍 Scanning 8,000,000,000 Humans...
+
+██████████
+
+🚨 ALERT!
+
+Suspicious Human Found...
+
+Name : Fouziya
+
+😂 Crime:
+Being too awesome.
+
+🎂 HAPPY BIRTHDAY
